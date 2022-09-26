858a8936e4cb3a1f3fbdb3f7a5d2ff02d23f9460
cakeforu <cakeforu@outlook.com>
2022-09-26 21:54:08 +0530

    created component to list & add user, added routing for these components, written html for nav bar in app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './components/user/add/add.component';
+import { ListComponent } from './components/user/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent},
+  { path: 'list', component: ListComponent},
+  { path: 'add', component: AddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
