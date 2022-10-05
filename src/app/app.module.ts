e7e56217450bb0dd7b9a1c502692067dbd747cfb
cakeforu <cakeforu@outlook.com>
2022-10-05 10:53:23 +0530

    bottstrap popup, json-server, removed bootstrap which installed using cmd
    Removed bootstrap which installed sing command prompt, Added bootstrap using link and script tag, installed json-server, added html bootstrap popup for add product, created api service, defined baseUrl const

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ListComponent } from './components/user/list/list.component';
 import { AddComponent } from './components/user/add/add.component';
 
