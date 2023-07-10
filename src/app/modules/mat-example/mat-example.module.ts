8675f79c1e9f956272bfde687bfb2ac44a4fef22
Avinash Jaiswal <Avinash.Jaiswal@users.noreply.github.com>
2023-07-10 23:27:37 +0530

    confirmation dialog component is created

--- src/app/modules/mat-example/mat-example.module.ts
+++ src/app/modules/mat-example/mat-example.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDashboardComponent } from './mat-dashboard/mat-dashboard.component';
 import { MatExampleRoutingModule } from './mat-example-routing.module';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 
 
 
 @NgModule({
   declarations: [
-    MatDashboardComponent
+    MatDashboardComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
     CommonModule,
